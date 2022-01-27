eb56f1202fe24dcb2d1337ba92ced418376ca43b
Gayrat Usmonov <usmonovgayrat89@gmail.com>
2022-01-27 13:47:43 +0500

    add changes

---
+++ main.go
@@ -0,0 +1,31 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+type bot interface {
+	getGreeting() string
+}
+
+type englishBot struct{}
+type spanishBot struct{}
+
+func (englishBot) getGreeting() string {
+	return "Hi There"
+}
+
+func (spanishBot) getGreeting() string {
+	return "Hola!"
+}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
